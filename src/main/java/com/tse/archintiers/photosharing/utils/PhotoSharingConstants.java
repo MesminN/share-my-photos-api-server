dcabd69a6d5981f5b5e0a30e00f592c06c5500a1
Mesmin NGOULO BEMBE <mesmin.ngoulo@gmail.com>
2022-02-20 19:13:12 +0100

    update

--- src/main/java/com/tse/archintiers/photosharing/utils/PhotoSharingConstants.java
+++ src/main/java/com/tse/archintiers/photosharing/utils/PhotoSharingConstants.java
@@ -4,5 +4,5 @@ public class PhotoSharingConstants {
 
     public final static String MEMBER = "MEMBER";
     public final static String GUEST = "GUEST";
-    public final static String IMAGES_BASE_PATH = "./src/main/resources/storedPhotos/";
+    public final static String IMAGES_BASE_PATH = "./";
 }
